8ef47962760b4bd3621a5e02f49177347ee32e3c
AleksandrPetrov89 <petrovav89@gmail.com>
2023-06-28 10:43:30 +0500

    Fixed task 1. The list is closed by pressing again

--- dropdown/task.js
+++ dropdown/task.js
@@ -10,15 +10,24 @@ function dropdownHandler (event) {
     // скрывает варианты всех других выпадающих списков.
     // При нажатии на один из вариантов списка устанавливает выбранное значение в основное поле и 
     // скрывает варианты всех выпадающих списков
-    for (let dropdown of dropdowns) {
-        dropdown.querySelector(".dropdown__list").className = "dropdown__list";
-    };
     if (event.target.className === "dropdown__value") {
         let dropdownList = event.currentTarget.querySelector(".dropdown__list");
-        dropdownList.className = "dropdown__list dropdown__list_active";
+        if (dropdownList.className.includes("dropdown__list_active")) {
+            hideList(dropdowns);
+        } else {
+            hideList(dropdowns);
+            dropdownList.className = "dropdown__list dropdown__list_active";
+        };
     } else if (event.target.className === "dropdown__link") {
         event.preventDefault();
+        hideList(dropdowns);
         let dropdownValue = event.currentTarget.querySelector(".dropdown__value");
         dropdownValue.textContent = event.target.textContent;
+    };
+};
+
+function hideList (collection) {
+    for (let dropdown of collection) {
+        dropdown.querySelector(".dropdown__list").className = "dropdown__list";
     };
 };
